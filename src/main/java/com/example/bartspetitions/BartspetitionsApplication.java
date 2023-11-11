067d646646eb973c64ee7888f7c4622f23b05ceb
Bartosz Perepeczo <bartosz.perepeczo@gmail.com>
2023-11-11 13:04:16 +0000

    Initial commit

---
+++ src/main/java/com/example/bartspetitions/BartspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bartspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BartspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BartspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bartspetitions/BartspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bartspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BartspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
